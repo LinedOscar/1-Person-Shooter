9da9494b09e541eb0999b6e6939564b07f6c3be8
Oscar Fällgren Glemme <omg_ogge@hotmail.com>
2020-05-26 05:48:50 +1000

    level, Booth prefabs and hologram rotation script

---
+++ Assets/Scripts/RotatingHologram.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingHologram : MonoBehaviour
+{
+
+    public float speed = 50.0f;
+    public bool local = true;
+
+    void Start()
+    {
+        
+
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        if(local == true)
+        {
+
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        }
+        
+        transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
+
+    }
+}
